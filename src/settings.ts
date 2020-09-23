af9d740b29180d7224b82775c3e6607ec775fcd1
WayneKP <37004336+WayneKP@users.noreply.github.com>
2020-09-24 09:58:38 +1000

    Update settings.ts

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'Homebridge-Homebridge-Lenovo-Plug';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'homebridge-Accessory-Homebridge-Lenovo-Plug';
+export const PLUGIN_NAME = 'homebridge-accessory-homebridge-lenovo-plug';
