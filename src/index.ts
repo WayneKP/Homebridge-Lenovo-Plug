6828fca5db446709ccdaec7f17622ec2a5a2c750
WayneKP <37004336+WayneKP@users.noreply.github.com>
2020-09-24 15:01:07 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge-homebridge-lenovo-plug';
 
-import { Homebridge-Homebridge-Lenovo-Plug } from './settings';
-import { Homebridge-Homebridge-Lenovo-Plug } from './platform'; 
+import { homebridge-homebridge-lenovo-plug } from './settings';
+import { homebridge-homebridge-lenovo-plug } from './platform'; 
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(Homebridge-Homebridge-Lenovo-Plug, HomebridgePlatform);
+  api.registerPlatform(homebridge-homebridge-lenovo-plug, HomebridgePlatform);
 }
