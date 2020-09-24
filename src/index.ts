9518378ea3eacd073e72ff9d039dbdc0c8b2c387
WayneKP <37004336+WayneKP@users.noreply.github.com>
2020-09-24 14:45:58 +1000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform'; 
+import { Lenovo-Plug } from './settings';
+import { HomebridgePlatform } from './platform'; 
 
 /**
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(Lenovo-Plug, HomebridgePlatform);
 }
